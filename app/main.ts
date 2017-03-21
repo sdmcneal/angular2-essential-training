c5f86294a9113a3dcba5eff97e51def5401a97f2
Scott McNeal <scott.mcneal@gmail.com>
2017-03-20 21:31:04 -0700

    finished styling a component lesson

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by scott on 3/20/17.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module'
+
+platformBrowserDynamic().bootstrapModule(AppModule);
